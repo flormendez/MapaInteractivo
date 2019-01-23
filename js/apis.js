1139e8f56dc3caa5797672e4741d16edca573220
Florencia <florenciamendez93@gmail.com>
2019-01-23 19:15:31 -0300

    Mitad Guia 2: Obtener coordenadas y mostrar marcador

---
+++ js/apis.js
@@ -0,0 +1,23 @@
+var mapa; // Mapa que vamos a modificar
+var centro = {
+  lat: -34.592868,
+  lng: -58.4199791
+};
+/* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
+
+// Inicializa el mapa con un valor de zoom y una locación en el medio
+function inicializarMapa() {
+  /* Modificá la variable mapa con el constructor Map().
+    Tendrás que asignarle un valor de zoom y
+    un centro igual a la variable posicionCentral. */
+  mapa = new google.maps.Map(document.getElementById("map"), {
+    center: centro,
+    zoom: 12
+  });
+
+  geocodificadorModulo.inicializar();
+  marcadorModulo.inicializar();
+  direccionesModulo.inicializar();
+  lugaresModulo.inicializar();
+  streetViewModulo.inicializar();
+}
